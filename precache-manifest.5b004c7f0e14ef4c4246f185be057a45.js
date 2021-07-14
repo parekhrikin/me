a4d06a1d495c5148f1baf98957b569a802caa39f
Rikin <parekh.r@northeastern.edu>
2021-07-14 18:10:36 +0530

    Updates

---
+++ precache-manifest.5b004c7f0e14ef4c4246f185be057a45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "470c653f55affd7e91ad",
+    "url": "/me/static/css/main.83ed8070.chunk.css"
+  },
+  {
+    "revision": "470c653f55affd7e91ad",
+    "url": "/me/static/js/main.470c653f.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "32b9d19b69dd3c8c301268b3ef7bc3e9",
+    "url": "/me/index.html"
+  }
+];
